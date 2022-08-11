81d28db090f08078be43edca8174564851319c27
Yasin ilkalp Arabacı <yasinilkalp_24@hotmail.com>
2022-08-11 16:53:29 +0300

    dependency injection

--- DependencyInjection.cs
+++ DependencyInjection.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using RedisService.Service;
 
 namespace RedisService
 {
     public static class DependencyInjection
     {
+        public static IServiceCollection AddMailSender(this IServiceCollection services)
+        {
+
+            services.AddSingleton<RedisServer>();
+            services.AddScoped<IRedisService, Service.RedisService>();
+
+            return services;
+        }
     }
 }
